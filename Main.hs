5b7e56fcb45b9e312dc35e6eda94a2aa575acf7e
Denys Fridman <fridman1992@mail.ru>
2015-12-13 21:11:11 +0200

    add example

---
+++ Main.hs
@@ -0,0 +1,51 @@
+module Main where
+
+
+
+
+import           Scrabble.Dictionary    (Dictionary, makeDictionary)
+import           Scrabble.Scrabble      (fromBonusBoard)
+import           Scrabble.ScrabbleAI    (showScrabble)
+import           Scrabble.ScrabbleParse (readBonusBoard)
+
+
+
+filename :: FilePath
+filename = "resources/4kwords.txt"
+
+
+
+
+readDictionary :: IO Dictionary
+readDictionary = readFile filename >>= return . makeDictionary
+
+
+
+bonusBoardStr :: String
+bonusBoardStr = unlines ["|||||||||||||||||",
+                         "|3..:...3...:..3|",
+                         "|.2...;...;...2.|",
+                         "|..2...:.:...2..|",
+                         "|:..2...:...2..:|",
+                         "|....2.....2....|",
+                         "|.;...;...;...;.|",
+                         "|..:...:.:...:..|",
+                         "|3..:...*...:..3|",
+                         "|..:...:.:...:..|",
+                         "|.;...;...;...;.|",
+                         "|....2.....2....|",
+                         "|:..2...:...2..:|",
+                         "|..2...:.:...2..|",
+                         "|.2...;...;...2.|",
+                         "|3..:...3...:..3|",
+                         "|||||||||||||||||"]
+
+
+
+main :: IO ()
+main = do
+  d <- readDictionary
+  case readBonusBoard bonusBoardStr of
+   Left err -> putStrLn err
+   Right b  -> let board = fromBonusBoard b in
+                showScrabble d board b >>= putStrLn
